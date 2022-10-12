d5246d46c166f90cc9ffae44596c2f32fb422460
tdemirors <timur.demirors@gmail.com>
2022-10-12 20:47:19 +0300

    Jenkins Build Profile

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -8,8 +8,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",  // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
         glue = {"StepDefinitions"},
-    //    plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
-        plugin= { "html:target//cucumber-reports.html"  }  // jenkins raporu için
+     plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+     //   plugin= { "html:target//cucumber-reports.html"  }  // jenkins raporu için
 )
 
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
